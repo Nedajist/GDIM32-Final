ac6d80c6884f5edf3b3cc0a36e563decb53f1dae
nathannnhernandez <personalhernandez17@gmail.com>
2026-02-24 17:47:49 -0800

    up

--- In Too Deep/Assets/Scripts/Checkpoint.cs
+++ In Too Deep/Assets/Scripts/Checkpoint.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour
 {
     [SerializeField] private int _currentCheckpointID;
+    [SerializeField] player _player;
     public Transform spawnPoint;
+    void Start()
+    {
+        spawnPoint = _player.transform;
+    }
     private void OnTriggerEnter(Collider isPlayer)
     {
         if (isPlayer.CompareTag("Player"))
