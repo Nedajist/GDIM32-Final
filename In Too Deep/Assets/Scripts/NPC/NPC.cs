aa3f597a02746f79fb628b192f13323e39931477
celotolosa <mttolosa@uci.edu>
2026-02-24 19:12:17 -0800

    NPC push
    created NPC parent class, included correct implementation for player script. Deleted playerinteract class. created questnpc and dialoguenpc

--- In Too Deep/Assets/Scripts/NPC/NPC.cs
+++ In Too Deep/Assets/Scripts/NPC/NPC.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-
-    void Start()
+    [SerializeField] protected string[] _dialogueLines;
+    protected virtual void Interact()
     {
-        
+        for (int i = 0; i < _dialogueLines.Length; i++)
+        {
+            Debug.Log(_dialogueLines[i]);
+        }
     }
 }
