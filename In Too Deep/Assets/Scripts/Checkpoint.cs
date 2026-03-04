5d5aa8abbde842e090e101644849b8832a344878
nathannnhernandez <ngherna2@uci.edu>
2026-03-04 13:01:25 -0800

    slope + tenmproary spawn

--- In Too Deep/Assets/Scripts/Checkpoint.cs
+++ In Too Deep/Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class CheckpointController : MonoBehaviour
 {
+    [SerializeField] private GameObject mushroomManIsland;
+
+    void Start()
+    {
+    /// <summary>
+    /// FOR TESTING PURPOSES ONLY
+    /// I am spawning the player at the mushroom man island, so I need to set the respawn point
+    /// </summary>
+        UIController ui = FindObjectOfType<UIController>();
+        ui.SetRespawnPoint(mushroomManIsland.transform);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
