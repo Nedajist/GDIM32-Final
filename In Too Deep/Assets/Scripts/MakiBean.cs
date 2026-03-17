d8e42f08f0e4e3e06d85cca9644e6d12f808a684
Nedajist <Kaimeng44@proton.me>
2026-03-16 19:12:05 -0700

    bugfix

--- In Too Deep/Assets/Scripts/MakiBean.cs
+++ In Too Deep/Assets/Scripts/MakiBean.cs
@@ -7,7 +7,6 @@ public class MakiBean : Interactable
 {
     [SerializeField] public float healing_seconds;
     [SerializeField] Rigidbody _rigidbody;
-    [SerializeField] AudioSource _audio_manager;
 
 
     [SerializeField] ParticleSystem _red_explosion;
@@ -22,7 +21,7 @@ public class MakiBean : Interactable
         Instantiate(_red_explosion, transform.position, Quaternion.identity);
         GameController.Instance.Player.get_rigidbody().AddForce(transform.forward * 1000 + transform.up * 600);
         GameController.Instance.Player.add_camera_shake(1);
-        _audio_manager.Play();
+        GameController.Instance.Player.BigDeathExplosion();
     }
 
 }
